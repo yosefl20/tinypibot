5c4bda9a962b184f9c1538849c84cb6ba2ae4305
Yosef Lin <lyz@erongdu.com>
2025-05-09 15:02:29 +0800

    update parameters

---
+++ pi_catkin_ws/dep-install.sh
@@ -0,0 +1,4 @@
+sudo apt install \
+  libopencv-dev libcv-bridge-dev libangles-dev libbullet-dev libimage-transport-dev \
+  libturbojpeg-dev libsdl1.2-dev libsdl-image1.2-dev libyaml-cpp-dev googletest google-mock \
+  libdiagnostic-updater-dev liborocos-bfl-dev libgtest-dev libbcm2835-dev pluginlib-dev
